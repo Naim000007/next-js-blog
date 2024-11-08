7f42d926d670c71262da4b0d0330100a401eb10e
Naim000007 <mdnaim01303202218@gmail.com>
2024-11-09 00:39:32 +0600

    front ui done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,27 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "../../public/hero.png";
+import Hero1 from "../../public/hero-img.webp";
+
 
 export default function Home() {
   return (
     <>
-
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>
+            Better design for your digital products.
+          </h1>
+          <p className={styles.desc}>
+            Transforming Visions into Reality. We bring together the teams from the
+            global tech industry.
+          </p>
+          <button className={styles.button}>See Our Work</button>
+        </div>
+        <div className={styles.item}>
+          <Image src={Hero1} alt="" className={styles.img} />
+        </div>
+      </div>
     </>
   );
 }
