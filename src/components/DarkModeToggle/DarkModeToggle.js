e57d942a5102903b3de00920f0de41f5e283902c
Naim000007 <mdnaim01303202218@gmail.com>
2024-11-09 11:15:40 +0600

    added darkmode and light mode functionality

--- src/components/DarkModeToggle/DarkModeToggle.js
+++ src/components/DarkModeToggle/DarkModeToggle.js
@@ -1,10 +1,13 @@
+"use client"
 import React from 'react'
 import styles from './DarkModeToggle.module.css'
+import { useContext } from 'react'
+import { ThemeContext } from '@/context/ThemeContext'
 
 const DarkModeToggle = () => {
-    const mode = "light"
+    const { toggle, mode } = useContext(ThemeContext)
     return (
-        <div className={styles.container} >
+        <div onClick={toggle} className={styles.container} >
             <div className={styles.icon}>🌙</div>
             <div className={styles.icon}>🔆</div>
             <div className={styles.ball} style={mode == "light" ? { left: "2px" } : { right: "2px" }} />
