9eca44bd1a1501f80d52029f813208f09bbda96b
Naim000007 <mdnaim01303202218@gmail.com>
2024-11-12 22:54:16 +0600

    show data from mongodb

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -3,16 +3,21 @@ import { NextResponse } from "next/server";
 import { writeFile } from "fs/promises";
 import BlogModel from "@/lib/model/BlogModel";
 
-
-
 //API endpoint to get all blog posts
 export async function GET(request) {
     await connectDB();
-    const blogs = await BlogModel.find({})
-    return NextResponse.json({ blogs });
+
+    const blogId = request.nextUrl.searchParams.get('id');
+    if (blogId) {
+        const blog = await BlogModel.findById(blogId)
+        return NextResponse.json(blog);
+    }
+    else {
+        const blogs = await BlogModel.find({})
+        return NextResponse.json({ blogs });
+    }
+
 }
-
-
 // API endpoint for uploading blog posts
 export async function POST(request) {
     await connectDB();
